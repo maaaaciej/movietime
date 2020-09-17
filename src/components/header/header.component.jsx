a6b5fd30aeee13fa606e4b48c735834a03ac8474
Maki <ostromaciej@gmail.com>
2020-09-17 14:17:57 +0200

    added some styling on the header, finished the wireframe for the app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./header.styles.scss";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <span className="header-title">MovieTime</span>
+    </div>
+  );
 };
 
 export default Header;
